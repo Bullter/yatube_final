f346a66c140c5a1363bc87f1a2c7e8f2c8ddce8e
Булат Каюмов <kaumovbulat@yandex.ru>
2022-12-10 10:49:03 +0300

    cloned project from hw04

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -31,6 +31,7 @@ class PostURLTests(TestCase):
             '/': 'posts/index.html',
             f'/group/{self.group.slug}/': 'posts/group_list.html',
             f'/profile/{self.user.username}/': 'posts/profile.html',
+            f'post/{self.post.id}/comment': 'posts/post_detail.html',
             f'/posts/{self.post.id}/': 'posts/post_detail.html',
             f'/posts/{self.post.id}/edit/': 'posts/create_post.html',
             '/create/': 'posts/create_post.html',
